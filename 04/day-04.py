c3fc1b975c332ac9778fd471c7ea765e8b29e944
Duane Littleton <dlittleton87@gmail.com>
2022-12-05 16:47:14 -0500

    Day 4, part 2

--- 04/day-04.py
+++ 04/day-04.py
@@ -4,10 +4,14 @@ def range_to_set(spec):
     lo, hi = map(int, spec.split('-'))
     return set(range(lo, hi + 1))
 
-count = 0
+subsets = 0
+overlaps = 0
 for line in map(str.rstrip, sys.stdin):
     a, b = map(range_to_set, line.split(','))
     if a.issubset(b) or b.issubset(a):
-        count += 1
+        subsets += 1
+    if not a.isdisjoint(b):
+        overlaps += 1
 
-print(count)
+print(subsets)
+print(overlaps)
