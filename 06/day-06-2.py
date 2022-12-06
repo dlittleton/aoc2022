1f7bb0b4e2b6b5ffa318b83692698aff07b53467
Duane Littleton <dlittleton87@gmail.com>
2022-12-06 08:16:00 -0500

    Day 6, part 2

---
+++ 06/day-06-2.py
@@ -0,0 +1,16 @@
+import sys
+
+def group(n, spec):
+    nspec = len(spec)
+    for i in range(nspec):
+        yield i, spec[i:i+n]
+
+size = 14
+spec = sys.stdin.readline()
+marker = 0
+for start, g in group(size, spec):
+    if len(set(g)) == size:
+        marker = start + size
+        break
+
+print(marker)
