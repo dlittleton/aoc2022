81f88c4a5e6110e287722937c5284f19ae451edf
Duane Littleton <dlittleton87@gmail.com>
2022-12-07 08:11:56 -0500

    Day 7, part 1

---
+++ 07/day-07.py
@@ -0,0 +1,60 @@
+import sys
+
+sep = '/'
+
+class Directory:
+
+    def __init__(self, path):
+        self.path = path
+        self.dirs = {}
+        self.files = {}
+
+    def add_record(self, spec):
+        if (spec.startswith('dir')):
+            _, name = spec.split(' ')
+            child = Directory(sep.join([self.path, name]))
+            self.dirs[name] = child
+            return child
+        else:
+            size, name = spec.split(' ')
+            self.files[name] = int(size)
+            return None
+
+    @property
+    def size(self):
+        files = sum(self.files.values())
+        dirs = sum(d.size for d in self.dirs.values())
+        return files + dirs
+
+pwd = ['/']
+root = Directory(pwd[0])
+all_dirs = {root.path: root}
+
+for l in map(str.rstrip, sys.stdin):
+    parts = l.split(' ')
+
+    if parts[0] == '$':
+        # Change directory
+        if parts[1] == 'cd':
+            if parts[2] == '/':
+                pwd = ['/']
+            elif parts[2] == '..':
+                pwd.pop()
+            else:
+                pwd.append(parts[2])
+        else:
+            # Do nothing for ls
+            continue
+    else:
+        current_path = sep.join(pwd)
+        current_dir = all_dirs[current_path]
+        new_dir = current_dir.add_record(l)
+        if new_dir:
+            all_dirs[new_dir.path] = new_dir
+
+total = 0
+for d in all_dirs.values():
+    if d.size <= 100000:
+        total += d.size
+
+print(total)
