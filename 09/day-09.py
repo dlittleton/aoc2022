b55b3ea949badfa266ad11b93ef216ca07628e39
Duane Littleton <dlittleton87@gmail.com>
2022-12-09 08:02:37 -0500

    Day 9, part 1

---
+++ 09/day-09.py
@@ -0,0 +1,47 @@
+import sys
+
+def sign(x):
+    if x == 0:
+        return 0
+    elif x > 0:
+        return 1
+    else:
+        return -1
+
+def move_tail(h, t):
+    tx, ty = t
+    diff_x = h[0] - tx
+    diff_y = h[1] - ty
+    
+    if abs(diff_x) > 1:
+        tx += sign(diff_x)
+        if (diff_y != 0):
+            ty += sign(diff_y)
+    elif abs(diff_y) > 1:
+        ty += sign(diff_y)
+        if (diff_x != 0):
+            tx += sign(diff_x)
+
+    return (tx, ty)
+
+
+head = (0, 0)
+tail = (0, 0)
+visited = set()
+
+for l in map(str.rstrip, sys.stdin):
+    dir, n = l.split(' ')
+    for i in range(int(n)):
+        if dir == 'R':
+            head = (head[0], head[1] + 1)
+        elif dir == 'U':
+            head = (head[0] - 1, head[1])
+        elif dir == 'L':
+            head = (head[0], head[1] - 1)
+        elif dir == 'D':
+            head = (head[0] + 1, head[-1])
+        
+        tail = move_tail(head, tail)
+        visited.add(tail)
+
+print(len(visited))
