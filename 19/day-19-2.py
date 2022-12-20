4b523bb3c91d7ec9ca7e6908158ea762062e5c18
Duane Littleton <dlittleton87@gmail.com>
2022-12-20 07:59:29 -0500

    Refactor to check max production first.

--- 19/day-19-2.py
+++ 19/day-19-2.py
@@ -68,34 +68,37 @@ def simulate(turns, idx, ore_ore, clay_ore, obs_ore, obs_clay, geode_ore, geode_
         elif pg > zg:
             best_for_time[t] = (po, pc, pob, pg)
 
-        
-
+        # Do nothing this turn
         heads.append((no, nc, nob, ng, bo, bc, bob, bg, nt))
 
-        if ao >= ore_ore:
-            max_use = max_ore * rt
-            avail = ao + (bo * rt)
-            if avail < max_use:
+        # Produce Ore Robot
+        ore_available = ao + (bo *rt)
+        ore_use = max_ore * rt
+        if ore_available < ore_use:
+            if ao >= ore_ore:
                 heads.append((no - ore_ore, nc, nob, ng, bo + 1, bc, bob, bg, nt))
-            else:
-                pruned['max_production'] += 1
+        else:
+            pruned['max_production'] += 1
 
-        if ao >= clay_ore:
-            max_use = obs_clay* rt
-            avail = ac + (bc * rt)
-            if avail < max_use:
+        # Produce Clay Robot
+        clay_available = ac + (bc *rt)
+        clay_use = obs_clay * rt
+        if clay_available < clay_use:
+            if ao >= clay_ore:
                 heads.append((no - clay_ore, nc, nob, ng, bo, bc + 1, bob, bg, nt))
-            else:
-                pruned['max_production'] += 1
+        else:
+            pruned['max_production'] += 1
 
-        if bob < geode_obs and ao >= obs_ore and ac >= obs_clay:
-            max_use = geode_obs * rt
-            avail = aob + (bob * rt)
-            if avail < max_use:
+        # Produce Obsidian Robot
+        obs_available = aob + (bob * rt)
+        obs_use = geode_obs * rt
+        if obs_available < obs_use:
+            if bob < geode_obs and ao >= obs_ore and ac >= obs_clay:
                 heads.append((no - obs_ore, nc - obs_clay, nob, ng, bo, bc, bob + 1, bg, nt))
-            else:
-                pruned['max_production'] += 1
+        else:
+            pruned['max_production'] += 1
 
+        # Produce Geode Robot
         if ao >= geode_ore and aob >= geode_obs:
             heads.append((no - geode_ore, nc, nob - geode_obs, ng, bo, bc, bob, bg + 1, nt))
 
